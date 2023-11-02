1a9dfdf68314d195be871a41f90187faa8aeadd4
T0n0T <823478402@qq.com>
2023-11-03 00:11:17 +0800

    update clangd

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
+#include <cstdlib>
 
-int test(int a, int b) { return a + b; }
-
-int main(int /*argc*/, const char ** /*argv*/) {
-  int c = test(3, 6);
-  std::cout << "Hello, from cpp-study!\n";
+int main(int argc, const char** argv) {
+    return 0;
 }
