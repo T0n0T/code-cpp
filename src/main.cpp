e2b9df5dd91ea4b8eb7d346989d87292a93eb260
T0n0T <823478402@qq.com>
2023-11-03 17:42:52 +0800

    feat: :zap: update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <cstdlib>
 
-int main(int argc, const char** argv) {
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+    string first_name, last_name;
+    cout << "Enter your first name: ";
+    cin >> first_name;
+    cout << "Enter your last name: ";
+    cin >> last_name;
+    cout << "here's the information in a single string: " << first_name << ", " << last_name << endl;
     return 0;
 }
